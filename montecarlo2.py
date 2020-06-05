b45ad5e92cd32f850b9ffd6072eee37f6f418922
AndreanaTW <49316987+AndreanaTW@users.noreply.github.com>
2020-06-05 21:56:25 +0700

    Add files via upload

---
+++ montecarlo2.py
@@ -0,0 +1,30 @@
+from scipy import random
+import numpy as np
+
+a = -1
+b = 1
+N=100
+n=3
+xrand=np.zeros(N)
+yrand=np.zeros(N)
+zrand=np.zeros(N)
+integral=0.0
+for i in range(n+1):
+    for i in range(len(xrand)):
+        xrand[i]=random.uniform(a,b)
+
+    for i in range(len(yrand)):
+        yrand[i]=random.uniform(a,b)
+
+    for i in range(len(zrand)):
+        zrand[i]=random.uniform(a,b)
+
+    def func(x,y,z):
+        return (x**2)+(y**2)+(z**2)
+
+
+    for i in range(N):
+        integral+=func(xrand[i],yrand[i],zrand[i])
+
+jawab=(b-a)/float(N)*integral
+print("Hasil : ",jawab)
